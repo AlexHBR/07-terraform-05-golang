2d4059b3285c17fc65f6d9a531b73560641982b7
Aleksey Zametaev <lexas2007@mail.ru>
2022-07-06 21:12:30 +0400

    first commit 1

--- main.go
+++ main.go
@@ -6,48 +6,41 @@ import (
 )
 
 func main() {
-	//sort_mas()
-	max_mas_no_sort()
-	//calc_fut_metr()
-
-	//delitel_3()
-
+	calc_fut_metr()
+	x := []int{48, 96, 86, 68, 57, 82, 63, 70, 37, 34, 83, 27, 19, 97, 9, 17}
+	fmt.Println("2. Наименьший элемент в любом заданном списке \n Список по умолчанию", x)
+	fmt.Println("Вариант решения 1 наименьший элемент в любом заданном списке: ", max_mas_no_sort(x))
+	fmt.Println("Вариант решения 2 наименьший элемент в любом заданном списке: ", sort_mas(x))
+	delitel_3()
 }
 
 func calc_fut_metr() {
-
-	fmt.Print("Enter a number: ")
+	fmt.Print("1. Введите  количество метров для перевода в футы: ")
 	var input float64
 	fmt.Scanf("%f", &input)
-
 	output := input * 0.3048
-
-	fmt.Println(output)
-
+	fmt.Println("соответсвует", output, " футов \n")
 }
 
-func sort_mas() {
-	x := []int{48, 96, 86, 68, 57, 82, 63, 70, 37, 34, 83, 27, 19, 97, 9, 17}
+func sort_mas(x []int) int {
 	sort.Ints(x)
-	fmt.Println(x[len(x)-1])
+	return x[len(x)-1]
 }
 
-func max_mas_no_sort() {
-	x := []int{48, 96, 86, 68, 57, 82, 63, 70, 37, 34, 83, 27, 19, 97, 9, 17}
+func max_mas_no_sort(x []int) int {
 	e_max := 0
-	for _, s := range x {
-		if e_max < s {
-			e_max = s
+	for _, i := range x {
+		if e_max < i {
+			e_max = i
 		}
 	}
-
-	fmt.Println(e_max)
+	return e_max
 }
 
 func delitel_3() {
-
+	fmt.Print("\n3.Числа от 1 до 100 делящиеся на 3 \n")
 	for a := 3; a <= 100; a = a + 3 {
-		fmt.Printf("value of a: %d\n", a)
-
+		fmt.Print(" ", a)
 	}
+	fmt.Println()
 }
